783266b9209bdc688665f0d2d66477a946eb8d7a
Sean Zhu <interestinglythere@gmail.com>
2016-07-14 22:51:37 -0700

    Create mcgimp-cc.rb

---
+++ Casks/mcgimp-cc.rb
@@ -0,0 +1,11 @@
+cask 'mcgimp-cc' do
+  version '2.9'
+  sha256 '0b89c8ba1eaedf7366699baac35e4d4d01ffd0763fea01a03508d3889beb243f'
+
+  url 'http://www.partha.com/downloads/McGimp-2.9-cc.app.zip'
+  name 'McGimp Color Patched'
+  homepage 'http://www.partha.com/'
+  license :gratis
+
+  app 'McGimp-2.9-cc.app'
+end
