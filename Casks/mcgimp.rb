0497a1f5964a1d902de662faef17cb9b528c5e06
Sean Zhu <interestinglythere@gmail.com>
2016-07-14 22:51:53 -0700

    Create mcgimp.rb

---
+++ Casks/mcgimp.rb
@@ -0,0 +1,11 @@
+cask 'mcgimp' do
+  version '2.9'
+  sha256 'fd98c0a31c719914ac05868a6366bab884c40c4504d04a55df6d263dbc5bfc02'
+
+  url 'http://www.partha.com/downloads/McGimp-2.9.app.zip'
+  name 'McGimp'
+  homepage 'http://www.partha.com/'
+  license :gratis
+
+  app 'McGimp-2.9.app'
+end
