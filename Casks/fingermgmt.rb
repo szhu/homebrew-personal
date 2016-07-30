aa9fb2f393707729d2cdee83fc88f9922a2bf0d5
Sean Zhu <interestinglythere@gmail.com>
2016-07-30 14:49:11 -0700

    Create fingermgmt.rb

---
+++ Casks/fingermgmt.rb
@@ -0,0 +1,11 @@
+cask 'fingermgmt' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://www.makeitgo.ws/articles/fingermgmt/FingerMgmt.zip'
+  name 'FingerMgmt'
+  homepage 'http://www.makeitgo.ws/articles/fingermgmt/'
+  license :oss
+
+  app 'FingerMgmt.app'
+end
