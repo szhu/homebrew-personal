e721cbea4aff4a720b95245efbca38061e5d181e
Sean Zhu <interestinglythere@gmail.com>
2016-09-12 03:10:28 -0700

    Create harmony.rb

---
+++ Casks/harmony.rb
@@ -0,0 +1,11 @@
+cask 'harmony' do
+  version '0.2.3'
+  sha256 '9ecafc902087d18382b5be0454fb0c3be0bc85a247e65743307cd728806e3ae8'
+
+  url "https://github.com/vincelwt/harmony/releases/download/#{version}/Harmony.app.zip"
+  name 'Harmony'
+  homepage 'http://getharmony.xyz/'
+  license :oss
+
+  app 'Harmony.app'
+end
