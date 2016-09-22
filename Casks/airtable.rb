a87cacf1c63fddd83828ae21354d0c5583695c94
Sean Zhu <interestinglythere@gmail.com>
2016-09-22 03:12:38 -0700

    Create airtable.rb

---
+++ Casks/airtable.rb
@@ -0,0 +1,11 @@
+cask 'airtable' do
+  version '2'
+  sha256 'd6883caadcdf6e3ea3d445beee33e11bc73c2eb26a7d0d753765455941968701'
+
+  url "https://static.airtable.com/download/AirtableInstaller.dmg?v=#{version}"
+  name 'Airtable'
+  homepage 'http://airtable.com/'
+  license :gratis
+
+  app 'Airtable.app'
+end
