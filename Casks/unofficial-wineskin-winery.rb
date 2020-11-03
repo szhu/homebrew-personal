65ebcb282daa61e217fd9748737c6ead31f709a3
Sean Zhu <interestinglythere@gmail.com>
2020-11-03 14:55:02 -0800

    Create unofficial-wineskin-winery.rb

---
+++ Casks/unofficial-wineskin-winery.rb
@@ -0,0 +1,11 @@
+cask "unofficial-wineskin-winery" do
+  version "1.8.4"
+  sha256 "902bc8d5c36b2c9b653251905d41b1e6685570500aece1636dbc47f412535a5d"
+
+  url "https://github.com/Gcenx/WineskinServer/releases/download/V1.8.4/Unofficial.Wineskin.Winery.zip"
+  name "Unofficial Wineskin Winery"
+  desc ""
+  homepage "https://github.com/Gcenx/WineskinServer"
+
+  app "Unofficial Wineskin Winery.app"
+end
