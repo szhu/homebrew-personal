b1986bda50b92961d0d82f3b4d9654473e35207c
Sean Zhu <interestinglythere@gmail.com>
2020-11-04 22:37:16 -0800

    Create airpods-sound-quality-fixer.rb

---
+++ Casks/airpods-sound-quality-fixer.rb
@@ -0,0 +1,11 @@
+cask "airpods-sound-quality-fixer" do
+  version "1.0"
+  sha256 "9c8be3dce5c87816d01b33feae175fd8e544642077e081d5b8d1d3b0e33c51d2"
+
+  url "https://github.com/milgra/airpodssoundqualityfixer/releases/download/1.0/AirPods.Sound.Quality.Fixer1.0.zip"
+  name "AirPods Sound Quality Fixer"
+  desc "Fixes sound quality drops when using AirPods with Macs. "
+  homepage "https://github.com/milgra/airpodssoundqualityfixer"
+
+  app "AirPods Sound Quality Fixer.app"
+end
