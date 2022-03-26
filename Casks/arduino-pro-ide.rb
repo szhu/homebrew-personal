d3aa3caa09707bda9c4aad62da93953faf17a46f
Sean Zhu <interestinglythere@gmail.com>
2022-03-26 14:25:04 -0400

    Update arduino-pro-ide.rb

--- Casks/arduino-pro-ide.rb
+++ Casks/arduino-pro-ide.rb
@@ -1,11 +1,11 @@
 cask "arduino-pro-ide" do
-  version "0.1.2"
-  sha256 "3fdf600c38a073817aa3183537067862e13a778f3e0531eefd0d3b68cbec65b7"
+  version "2.0.0-rc5"
+  sha256 :no_check
 
-  url "https://downloads.arduino.cc/arduino-pro-ide/arduino-pro-ide_#{version}_macOS_64bit.dmg"
-  name "Arduino Pro IDE"
+  url "https://downloads.arduino.cc/arduino-ide/arduino-ide_#{version}_macOS_64bit.dmg"
+  name "Arduino IDE 2.0 Beta"
   desc "The Arduino IDE for advanced users and developers."
-  homepage "https://www.arduino.cc/pro/arduino-pro-ide"
+  homepage "https://www.arduino.cc/en/software#experimental_software"
 
-  app "Arduino Pro IDE.app"
+  app "Arduino IDE.app"
 end
